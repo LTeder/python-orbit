42a06fc1f0be47b3f0cfe17168f992593df31c61
EvilMossman <lteder01@gmail.com>
2016-06-02 22:15:06 -0400

    Starting option grabbing for items
    Not done atm, so it doesn't work [ofc]
    A function is called that spawns an item with parameters that are
    displayed as a rotated ellipse (using orbitmath.py) and a moving sphere
    (I have no idea how the hell that's going to happen) using a dictionary
    (1: [14, 15, 5123])

--- orbit.py
+++ orbit.py
@@ -1,4 +1,6 @@
 import Tkinter as tk
+import orbitmath
+import random
 
 
 class Orbit(tk.Frame):
@@ -6,32 +8,47 @@ class Orbit(tk.Frame):
         tk.Frame.__init__(self, root)
         self.width = 500
         self.height = 500
+        self.scale = 50
+        self.large = self.scale * [self.width if self.width > self.height else self.height]
         self.canvas = tk.Canvas(self, width=self.width, height=self.height, background='black')
         self.options = tk.Frame(self, height=self.height)
-        tk.Label(self.options, text="Press RETURN to confirm a value.").pack()
+        tk.Label(self.options,
+                 text="Confirm value: Return.\nUnits: Light Seconds.\n1 Pixel = %s Light Seconds." % self.scale).pack()
 
         self.options.grid(row=0, column=1, sticky='nsew')
         self.canvas.grid(row=0, column=0, sticky='nsew')
 
         # I plan to eventually put window size configs (hence the computations for core object size)
+        self.bodies = {}
         self.sun = self.canvas.create_oval((self.width / 2) - 10, (self.height / 2) + 10,
                                            (self.width / 2) + 10, (self.height / 2) - 10, fill='yellow')
 
         self.canvas.bind('<ButtonRelease-1>', self.select)
 
-        self.base_options = [
-            ("Test:", 42),
-            ("Test 2:", 243)]
+        self.base_options = ["Scale:", "Test 2:"]
 
-        self.item_options = [
-            ("Mass:", 1000000),
-            ("Density:", 23)]
+        self.item_options = ["Apoapsis:", "Periapsis:", "Arg. of Periapsis:"]
+
+        self.item = 0
+
+        self.option_frames = []
+        self.option_items = []
+
+    def spawn_item(self, apoaps=None, periaps=None,
+                   argp=random.randint(0, 359)):  # Creates the canvas item, ellipse, and dictionary for each item
+        if apoaps is None:
+            apoaps = random.randint(2, self.large)
+        if periaps is None:
+            periaps = random.randint(1, apoaps)
+        focus_displacement = (apoaps + periaps) / 2
+        self.bodies[self.canvas.create_line(orbitmath.poly_oval())] = [apoaps, periaps, argp]
+        pass
 
     def select(self, event):
         try:
             self.item = self.canvas.find_overlapping(event.x - 1, event.y + 1, event.x + 1, event.y - 1)[0]
         except IndexError:  # For when there is no self.item in the click zone
-            self.item = 0  # Overall options for the sim (maybe save/load)
+            pass  # Overall options for the sim (maybe save/load)
         try:
             for frame in self.option_frames:  # Clearing option frames
                 frame.pack_forget()
@@ -40,8 +57,6 @@ class Orbit(tk.Frame):
                 option_pair[1].pack_forget()
         except AttributeError:
             pass  # The first run won't have these variables in place
-        self.option_frames = []  # Resetting variables
-        self.option_items = []
         if self.item == 0:
             option_set = self.base_options  # Sets which set of options to use
         else:
@@ -50,11 +65,11 @@ class Orbit(tk.Frame):
         for option in option_set:  # Parses through option data
             self.option_frames.append(tk.Frame(self.options))
             self.option_frames[count].pack()
-            self.option_items.append((tk.Label(self.option_frames[count], text=option[0]),
+            self.option_items.append((tk.Label(self.option_frames[count], text=option),
                                       tk.Entry(self.option_frames[count])))
             self.option_items[count][0].pack(side=tk.LEFT)
             self.option_items[count][1].bind('<Return>', self.param_update)
-            self.option_items[count][1].insert(0, self.item[option[0].cget('text')])
+            self.option_items[count][1].insert(0, self.item[self.option_items[count][0].cget('text')])
             self.option_items[count][1].pack(side=tk.RIGHT)
             count += 1
 
@@ -63,7 +78,6 @@ class Orbit(tk.Frame):
             if item[0] == event:
                 option = item
         self.item[option[0].cget('text')] = float(option[1].get('1.0', 'end-1c'))
-        print option
 
 
 if __name__ == '__main__':
